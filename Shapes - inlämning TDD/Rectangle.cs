b50496604acd08adbbbf3bcd9c6d45eaaef33071
Benny Christensen <benny@potatismoose.com>
2021-04-14 15:17:26 +0200

    Add project files.

---
+++ Shapes - inlämning TDD/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes___inlämning_TDD
+{
+    class Rectangle
+    {
+        public Rectangle(float height, float width)
+        {
+            Height = height;
+            Width = width;
+        }
+        public float Height { get; set; }
+        public float Width { get; set; }
+
+        internal float CalculateArea(Rectangle rectangle)
+        {
+            if (rectangle.Height < 0 || rectangle.Width < 0)
+            {
+                return 0;
+            }
+
+            return rectangle.Height * rectangle.Width;
+        }
+    }
+}
---
+++ Shapes - inlämning TDD/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Shapes___inlämning_TDD
+{
+    internal class Square
+    {
+        public Square(float side)
+        {
+            Side = side;
+        }
+
+        public float Side { get; set; }
+
+        internal float CalculateArea(Square square)
+        {
+            const float ToThePowerOf = 2.0f;
+
+            if (square.Side < 0)
+            {
+                return 0;
+            }
+            return MathF.Pow(square.Side, ToThePowerOf);
+        }
+    }
+}
